93ae78e3f3b5c5890c7e232d93015122f27ac58d
l.aminov <l.aminov@tinkoff.ru>
2019-05-07 08:55:55 +0500

    Add prom version

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+
+	"github.com/prometheus/common/version"
 
 	"github.com/sirupsen/logrus"
 )
@@ -10,16 +13,24 @@ import (
 var (
 	configFlag   = flag.String("config", "/opt/prometheus-actions/prometheus-actions.yaml", "Path to configuration file")
 	logLevelFlag = flag.String("log-level", "debug", "Logging level")
+	versionFlag  = flag.Bool("version", false, "Prints version and exit")
 )
 
 func realMain() int {
 	flag.Parse()
+
+	if *versionFlag {
+		fmt.Println(version.Print("prometheus-actions"))
+		return 0
+	}
+
 	log := logrus.New()
 	if level, err := logrus.ParseLevel(*logLevelFlag); err == nil {
 		log.SetLevel(level)
 	}
 	log.Formatter = &logrus.JSONFormatter{}
-	log.Info("Starting prometheus-actions...")
+	log.Infof("Starting prometheus-actions %s...", version.Version)
+
 	config, err := LoadConfig(*configFlag)
 	if err != nil {
 		log.Error(err)
@@ -29,6 +40,7 @@ func realMain() int {
 		log.Error(err)
 		return 1
 	}
+
 	executor, err := NewExecutor(log, config)
 	if err != nil {
 		log.Error(err)
@@ -38,6 +50,7 @@ func realMain() int {
 		log.Error(err)
 		return 1
 	}
+
 	return 0
 }
 
