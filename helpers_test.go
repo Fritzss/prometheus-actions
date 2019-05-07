73572ed97e7b1abad48159aa568e6511a0e0867e
l.aminov <l.aminov@tinkoff.ru>
2019-05-07 12:30:45 +0500

    Add test

--- helpers_test.go
+++ helpers_test.go
@@ -12,4 +12,14 @@ func TestStandardizeSpaces(t *testing.T) {
 	if out != "line 1 line 2 line 3" {
 		t.Errorf("Failed match string: %s", out)
 	}
+	input = `
+	(
+        node_filesystem_free{instance="localhost", mountpoint="/var/lib/docker"} /
+        node_filesystem_size{instance="localhost", mountpoint="/var/lib/docker"}
+	) * 100 < 100
+	`
+	out = StandardizeSpaces(input)
+	if out != `( node_filesystem_free{instance="localhost", mountpoint="/var/lib/docker"} / node_filesystem_size{instance="localhost", mountpoint="/var/lib/docker"} ) * 100 < 100` {
+		t.Errorf("Failed match string: %s", out)
+	}
 }
